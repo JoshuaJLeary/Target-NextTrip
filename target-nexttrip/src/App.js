6bbb768bc9e98d0f9fb724865ebb1a1e60f025f0
Joshua Leary <joshua@modernlogic.io>
2019-11-26 10:04:34 -0600

    NextTrip home screen css applied

--- target-nexttrip/src/App.js
+++ target-nexttrip/src/App.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <form className='headerContainer'>
+          <div className='headerContainer'>
+            <div id='metroImgContainer'>
+              <img src='https://www.metrotransit.org/images/mob_logo.png' className='metroImg' alt='metroTransitLogo' />
+            </div>
+            <div id='metroImgBannerContainer'>
+              <img src='https://www.metrotransit.org/images/mob_banner.png' className='metroImgBanner' alt='metroTransitBanner' />
+            </div>
+          </div>
+          <div className='inputSelectContainer'>
+            <select className='inputSelect'>
+              <option>
+                'Test'
+              </option>
+              <option>
+                'Test 2'
+              </option>
+              <option>
+                'Test 3'
+              </option>
+              <option>
+                'Test 4'
+              </option>
+            </select>            
+          </div>
+          <div className='inputSelectContainer'>
+            <select className='inputSelect'>
+              <option>
+                'Test'
+              </option>
+              <option>
+                'Test 2'
+              </option>
+              <option>
+                'Test 3'
+              </option>
+              <option>
+                'Test 4'
+              </option>
+            </select>
+         </div>
+         <div className='inputSelectContainer'>
+            <select className='inputSelect'>
+              <option>
+                'Test'
+              </option>
+              <option>
+                'Test 2'
+              </option>
+              <option>
+                'Test 3'
+              </option>
+              <option>
+                'Test 4'
+              </option>
+            </select>
+          </div>
+          <hr className='dividerLine' />
+          <div className='inputStopNumberContainer'>
+            <input className='inputStopNumber' type='text'>
+            </input>
+          </div>
+          <div className='inputStopNumberContainer'>
+            <input className='inputStopNumberBtn' type='submit' value='STOP NUMBER'>
+            </input>
+          </div>
+        </form>
+      </body>
     </div>
   );
 }
